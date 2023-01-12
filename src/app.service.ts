fc0156cbeb01c4e46dc6703c4b464b5088eb043e
taaddde <taaddde@hotmail.com>
2023-01-12 12:38:58 -0300

    [FT] Services init

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,46 @@
 import { Injectable } from '@nestjs/common';
+import { Location } from './models';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  async getLocation(ip: string): Promise<Location> {
+    const response = await fetch(`http://ip-api.com/json/${ip}`)
+    const data = await response.json()
+
+    return data;
+  }
+
+  async getCurrent(ip: string, city?: string): Promise<any> {
+    let url = '';
+    if (city) {
+      url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.OPENWEATHER_KEY}&units=metric`
+    } else {
+      const response = await fetch(`http://ip-api.com/json/${ip}`)
+      const {lat, lon} = await response.json()
+  
+      url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.OPENWEATHER_KEY}&units=metric`
+    }
+
+    const response = await fetch(url)
+    const data = await response.json()
+
+    return data
+  }
+
+  async getForecast(ip: string, city?: string): Promise<any> {
+    let url = '';
+    if (city) {
+      url = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${process.env.OPENWEATHER_KEY}&units=metric`;
+    } else {
+      const response = await fetch(`http://ip-api.com/json/${ip}`)
+      const {lat, lon} = await response.json()
+  
+      url = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${process.env.OPENWEATHER_KEY}&units=metric`;
+    }
+
+    const response = await fetch(url)
+    const data = await response.json()
+
+    return data
   }
 }
