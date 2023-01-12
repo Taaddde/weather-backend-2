16844a7ae6051c0c0b380757f04463397e606e1d
taaddde <taaddde@hotmail.com>
2023-01-12 12:41:04 -0300

    [FT] Controller init

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,34 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Req } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Location } from './models';
 
-@Controller()
+@Controller('v1')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('/location')
+  async getLocation(
+    @Req() request
+  ): Promise<Location> {
+    const {ip} = request;
+    return await this.appService.getLocation(ip);
+  }
+
+  @Get('/current/:city?')
+  async getCurrent(
+    @Req() request,
+    @Param('city') city?:string
+  ): Promise<any> {
+    const {ip} = request;
+    return await this.appService.getCurrent(ip, city);
+  }
+
+  @Get('/forecast/:city?')
+  async getForecast(
+    @Req() request,
+    @Param('city') city?:string
+  ): Promise<any> {
+    const {ip} = request;
+    return await this.appService.getForecast(ip, city);
   }
 }
