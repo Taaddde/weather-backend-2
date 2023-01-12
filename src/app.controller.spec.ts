4d1ea0e3b0d7071de8d5697513a862575b16a58c
taaddde <taaddde@hotmail.com>
2023-01-12 12:46:00 -0300

    [FT] More tests for /location

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,9 +15,17 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', async () => {
+    it('Correctly detects the location by ipv4', async () => {
       const response = await appController.getLocation({ip: '191.84.204.141'});
       expect(response.status).toBe('success');
+    });
+    it('Correctly detects the location by ipv6', async () => {
+      const response = await appController.getLocation({ip: '3c92:143c:4d6b:38aa:7a70:cf79:955d:e553'});
+      expect(response.status).toBe('success');
+    });
+    it('Failure to detect a location with the wrong ip', async () => {
+      const response = await appController.getLocation({ip: 'Mi ip'});
+      expect(response.status).toBe('fail');
     });
   });
 });
