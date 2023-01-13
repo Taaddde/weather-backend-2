b77f19761c0482ad1161e28d5b8421e950370030
taaddde <taaddde@hotmail.com>
2023-01-13 01:42:13 -0300

    [FX] Better e2e tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -5,6 +5,9 @@ import { AppModule } from './../src/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
+  const ipv4 = '191.84.204.141';
+  const ipv6 = '3c92:143c:4d6b:38aa:7a70:cf79:955d:e553';
+  const wrongIp = 'My ip';
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -16,9 +19,27 @@ describe('AppController (e2e)', () => {
   });
 
   describe('GET /v1/location', () => {
-    it('Location return 200 correctly', async () => {
+    it('Location return 200 correctly with ipv4', async () => {
       const startTime = performance.now();
-      const response = await request(app.getHttpServer()).get('/v1/location');
+      const response = await request(app.getHttpServer()).get('/v1/location').set('x-forwarded-for', ipv4)
+      expect(response.statusCode).toBe(200);
+      expect(response.headers['content-type']).toBe(
+        'application/json; charset=utf-8',
+      );
+      expect(performance.now() - startTime).toBeLessThan(800);
+    });
+    it('Location return 200 correctly with ipv6', async () => {
+      const startTime = performance.now();
+      const response = await request(app.getHttpServer()).get('/v1/location').set('x-forwarded-for', ipv6)
+      expect(response.statusCode).toBe(200);
+      expect(response.headers['content-type']).toBe(
+        'application/json; charset=utf-8',
+      );
+      expect(performance.now() - startTime).toBeLessThan(800);
+    });
+    it('Location return 404 correctly with wrong ip', async () => {
+      const startTime = performance.now();
+      const response = await request(app.getHttpServer()).get('/v1/location').set('x-forwarded-for', wrongIp)
       expect(response.statusCode).toBe(200);
       expect(response.headers['content-type']).toBe(
         'application/json; charset=utf-8',
@@ -32,7 +53,7 @@ describe('AppController (e2e)', () => {
       const startTime = performance.now();
       const response = await request(app.getHttpServer()).get(
         '/v1/current/Montevideo',
-      );
+      ).set('x-forwarded-for', ipv4);
       expect(response.statusCode).toBe(200);
       expect(response.headers['content-type']).toBe(
         'application/json; charset=utf-8',
@@ -42,7 +63,7 @@ describe('AppController (e2e)', () => {
 
     it('Current without city return 200 correctly', async () => {
       const startTime = performance.now();
-      const response = await request(app.getHttpServer()).get('/v1/current');
+      const response = await request(app.getHttpServer()).get('/v1/current').set('x-forwarded-for', ipv4);
       expect(response.statusCode).toBe(200);
       expect(response.headers['content-type']).toBe(
         'application/json; charset=utf-8',
@@ -56,7 +77,7 @@ describe('AppController (e2e)', () => {
       const startTime = performance.now();
       const response = await request(app.getHttpServer()).get(
         '/v1/forecast/Montevideo',
-      );
+      ).set('x-forwarded-for', ipv4);
       expect(response.statusCode).toBe(200);
       expect(response.headers['content-type']).toBe(
         'application/json; charset=utf-8',
@@ -66,7 +87,7 @@ describe('AppController (e2e)', () => {
 
     it('Forecast without city return 200 correctly', async () => {
       const startTime = performance.now();
-      const response = await request(app.getHttpServer()).get('/v1/forecast');
+      const response = await request(app.getHttpServer()).get('/v1/forecast').set('x-forwarded-for', ipv4);
       expect(response.statusCode).toBe(200);
       expect(response.headers['content-type']).toBe(
         'application/json; charset=utf-8',
